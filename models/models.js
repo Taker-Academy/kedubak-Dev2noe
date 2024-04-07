a6d0c141b7f9f7e65469c15f997867f714c8469e
noe.merle <noe.merle@epitech.eu>
2024-04-07 15:55:49 +0200

    add post schema

--- models/models.js
+++ models/models.js
@@ -33,3 +33,38 @@ const user_schema = new mongoose.Schema({
 
 const User = mongoose.model('User', user_schema);
 module.exports = User;
+
+const post_schema = new mongoose.Schema({
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+    userId: {
+        type: String,
+        require: true,
+    },
+    firstName: {
+        type: String,
+        require: true,
+    },
+    title: {
+        type: String,
+        require: true,
+    },
+    content: {
+        type: String,
+        require: true,
+    },
+    comments: {
+        type: Array,
+        createdAt: {
+            type: Date,
+            default: Date.now,
+        },
+        id: {
+            type: String,
+            require: true,
+        },
+    },
+
+})
