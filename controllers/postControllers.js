3d63a0b130aecab44a5bc31c113a1917561f7a7b
noe.merle <noe.merle@epitech.eu>
2024-04-08 11:07:04 +0200

    add getUserPost and getPostById

--- controllers/postControllers.js
+++ controllers/postControllers.js
@@ -4,12 +4,6 @@ const User = require('../models/userModel.js');
 exports.getPosts = async (req, res) => {
     try {
         const post = await Post.find({}).lean().exec();
-        if (post.length === 0) {
-            return res.status(200).json({
-                ok: true,
-                data: [] // Renvoie un tableau vide si aucun post n'est trouvé
-            });
-        }
         const formattedPosts = post.map(post => ({
             _id: post._id,
             createdAt: post.createdAt,
@@ -27,7 +21,7 @@ exports.getPosts = async (req, res) => {
 
         res.status(200).json({
             ok: true,
-            data: formattedPosts
+            data: (formattedPosts === null || formattedPosts === undefined) ? [] : formattedPosts
         });
     } catch (error) {
         console.error(error);
@@ -55,6 +49,38 @@ exports.createPost = async (req, res) => {
         res.status(201).json({
             ok: true,
             data: newPost
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ ok: false, message: 'Erreur interne du serveur.' });
+    }
+}
+
+exports.getUserPosts = async (req, res) => {
+    try {
+        const posts = await Post.find({ userId: req.user.userId }).lean().exec();
+        res.status(200).json({
+            ok: true,
+            data: (posts === null || posts === undefined) ? [] : posts
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ ok: false, message: 'Erreur interne du serveur.' });
+    }
+}
+
+exports.getPostById = async (req, res) => {
+    try {
+        if (!req.params.id) {
+            return res.status(400).json({ ok: false, message: 'Paramètres manquant ou invalides' });
+        }
+        const post = await Post.findById(req.params.id).lean().exec();
+        if (!post) {
+            return res.status(404).json({ ok: false, message: 'Post non trouvé.' });
+        }
+        res.status(200).json({
+            ok: true,
+            data: post
         });
     } catch (error) {
         console.error(error);
--- routes/post.js
+++ routes/post.js
@@ -6,6 +6,7 @@ const postControllers = require('../controllers/postControllers.js');
 // Route pour récupérer tous les posts
 router.get('/', authenticateJWT, postControllers.getPosts);
 router.post('/', authenticateJWT, postControllers.createPost);
-//router.post('/me', authenticateJWT, postControllers.createPost);
+router.get('/me', authenticateJWT, postControllers.getUserPosts);
+router.get('/:id', authenticateJWT, postControllers.getPostById);
 
 module.exports = router;
